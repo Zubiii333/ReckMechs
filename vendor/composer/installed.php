e117e713676f3f132e6a553f2613fa242b05ab61
A K M Jubaeir Islam <jubaeir.islam37@gmail.com>
2025-08-08 16:23:48 +0600

    Hosting config fixed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => 'car-workshop/appointment-system',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '44542189e073648443cea00c00ce9de5d9104b77',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         'car-workshop/appointment-system' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '44542189e073648443cea00c00ce9de5d9104b77',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
